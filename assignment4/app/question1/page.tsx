0d28a821bcbed77eedb46ad35d20c9e13db072a7
Aditi Pandey <aditip149209@gmail.com>
2025-10-17 14:50:43 +0530

    more renames

---
+++ assignment4/app/question1/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Question1Page() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Question1Page
---
+++ assignment4/app/question2/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Question2Page() {
+  return (
+    <div>Question2Page</div>
+  )
+}
+
+export default Question2Page
